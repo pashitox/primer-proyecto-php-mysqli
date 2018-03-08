700e522bdfc2bde9e267e7ed7c2f1c3434442da0
pashitox <pashitox@gmail.com>
2018-03-08 11:32:20 -0300

    terminando el from

---
+++ php_action/Conexion.php
@@ -0,0 +1,9 @@
+<?php
+$dsn='mysql:host=localhost;dbname=login'; //significa Data Source Name o Nombre del Origen de los Datos
+$usuario='root';
+$password='';
+
+$conn = new PDO($dsn,$usuario,$password);
+
+
+  ?>
--- php_action/registar.php
+++ php_action/registar.php
@@ -1,22 +1,24 @@
 <?php
 
-move_uploaded_file($_FILES["imagen"]["tmp_name"],"fotografias/" . $_FILES["imagen"]["name"]);			
+include("conexion.php");
+
+move_uploaded_file($_FILES["imagen"]["tmp_name"],"../fotos" . $_FILES["imagen"]["name"]);			
 $imagen=$_FILES["imagen"]["name"];
 $nombre=$_POST['nombre'];
 $pellido=$_POST['apellido'];
-$ocupacio=$_POST['ocupacio'];
+
 $ND=$_POST['ND'];
 $fechaN=$_POST['fechaN'];
 $nacionalidad=$_POST['nacionalidad'];
 $direccion=$_POST['direccion'];
 $ciudad=$_POST['ciudad'];
-$telefono=$_POST['fax'];
+$telefono=$_POST['telefono'];
 $email=$_POST['email'];
 $pais=$_POST['pais'];
 $direccionA=$_POST['direccionA'];
 $tipoA=$_POST['tipoA'];
-$Fechai=$_POST['fechai'];
-$Fechaf=$_POST['fechaf'];
+$Fechai=$_POST['Fechai'];
+$Fechaf=$_POST['Fechaf'];
 $observ=$_POST['observ'];
 $tipop=$_POST['tipop'];
 $pago=$_POST['pago'];
@@ -25,9 +27,9 @@ $precio=$_POST['precio'];
 $existencia=$_POST['existencia'];
 
 $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-$sql = "INSERT INTO clientes (imagen, nombre, apellido, ocupacion, ND, 
+$sql = "INSERT INTO clientes (imagen, nombre, apellido, ND, 
 fechaN, nacionalidad, direccion, ciudad, telefono, email, pais, direccionA, tipoA, Fechai, fechaf, observ, tipop, pago)
-VALUES ('$imagen', '$nombre', '$apelllido','$ocupacion','$ND', '$fechaN', '$nacionalidad','$direccion',
+VALUES ('$imagen', '$nombre', '$apellido','$ND', '$fechaN', '$nacionalidad','$direccion',
 '$ciudad', '$telefono', '$email','$pais','$direccionA', '$tipoA', '$Fechai','$Fechaf','$observ', '$tipop', '$pago')";
 
 $conn->exec($sql);
