951f0c70bec524a7d76a0c78e93920b2d63b1d07
pashitox <pashitox@gmail.com>
2018-03-11 11:05:58 -0300

    validando registro pasaporte

--- php_action/registar.php
+++ php_action/registar.php
@@ -25,7 +25,29 @@ $tipop=$_POST['tipop'];
 $pago=$_POST['pago'];
 
 
+
 $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+$sentencia="select * from cliente where ND='".$ND."'";
+    $sql=$conn->prepare($sentencia); // con el metodo prepare del objeto conexion se prepara la consulta para ser ejecut
+    $sql->execute(); //ejecutar la sentencia invocando al metodo execute()
+    $resultado=$sql->fetchAll(); // la variable resultado es un arreglo que contiene todas las filas obtenidas de la sentencia 
+    $totalregistros=count($resultado); 
+    if ($totalregistros>0) //Es porque existen registros
+    { 
+        foreach ($resultado as $fila) //la variable $fila es un registro en particular obtenido del arreglo
+        {
+         $bdND=$fila['ND'];
+         
+        }
+        if($ND==$bdND) // SHA: Security Hash Algorithm 
+        {
+          echo "<script>alert('¡el cliente, esta en el sistema ...!'); window.location='../pages/forms.html';</script>";  
+        }
+        else
+        {
+        
+
 $sql = "INSERT INTO cliente (imagen, nombre, apellido, ocupacio, ND, 
 fechaN, nacionalidad, direccion, ciudad, telefono, email, pais, direccionA, tipoA, fechai, fechaf, observ, tipop, pago)
 VALUES ('$imagen', '$nombre', '$apellido', '$ocupacio', '$ND', '$fechaN', '$nacionalidad','$direccion',
@@ -33,7 +55,7 @@ VALUES ('$imagen', '$nombre', '$apellido', '$ocupacio', '$ND', '$fechaN', '$naci
 
 $conn->exec($sql);
 echo "<script>alert('Successfully Added!!!');window.location='../pages/forms.html'</script>";
-// }
+}
 
 // }
 ?>
