498a1cd0062368d1616918ac9d38a6dcff6d2c5a
pashitox <pashitox@gmail.com>
2018-03-11 17:52:31 -0300

    listo template

--- TEMPLATE.php
+++ TEMPLATE.php
@@ -160,11 +160,11 @@ echo $f["suma"];
         </div>
         <!-- /.row -->
         <div class="row">
-            <div class="col-lg-3 col-md-6">
+            <div class="col-lg-4 col-md-6">
                 <div class="panel panel-primary">
                     <div class="panel-heading">
                         <div class="row">
-                            <div class="col-xs-3">
+                            <div class="col-xs-4">
                                 <i class="fa fa-comments fa-5x"></i>
                             </div>
                             <div class="col-xs-11 text-right">
@@ -185,34 +185,12 @@ echo $f["suma"];
                     </a>
                 </div>
             </div>
-            <div class="col-lg-3 col-md-6">
-                <div class="panel panel-green">
-                    <div class="panel-heading">
-                        <div class="row">
-                            <div class="col-xs-3">
-                                <i class="fa fa-tasks fa-5x"></i>
-                            </div>
-                            <div class="col-xs-11 text-right">
-                                <div class="huge"> </div>
-                                <div>New Tasks!</div>
-                            </div>
-                        </div>
-                    </div>
-                    <a href="#">
-                        <div class="panel-footer">
-                            <span class="pull-left">View Details</span>
-                            <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
-
-                            <div class="clearfix"></div>
-                        </div>
-                    </a>
-                </div>
-            </div>
-            <div class="col-lg-3 col-md-6">
+           
+            <div class="col-lg-4 col-md-7">
                 <div class="panel panel-yellow">
                     <div class="panel-heading">
                         <div class="row">
-                            <div class="col-xs-3">
+                            <div class="col-xs-4">
                                 <i class="fa fa-shopping-cart fa-5x"></i>
                             </div>
                             <div class="col-xs-11 text-right">
@@ -233,11 +211,11 @@ echo $f["suma"];
                     </a>
                 </div>
             </div>
-            <div class="col-lg-3 col-md-6">
+            <div class="col-lg-4 col-md-6">
                 <div class="panel panel-red">
                     <div class="panel-heading">
                         <div class="row">
-                            <div class="col-xs-3">
+                            <div class="col-xs-4">
                                 <i class="fa fa-support fa-5x"></i>
                             </div>
                             <div class="col-xs-11 text-right">
